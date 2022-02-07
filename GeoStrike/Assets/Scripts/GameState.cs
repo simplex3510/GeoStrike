97d7d49ddb6d04efd44e2187e05ba538773f3687
Sandor2889 <97664446+Sandor2889@users.noreply.github.com>
2022-02-07 21:51:12 +0900

    0207 Project 작업
    UnitState 작업 중, Unit Move 테스트 진행

--- GeoStrike/Assets/Scripts/GameState.cs
+++ GeoStrike/Assets/Scripts/GameState.cs
@@ -21,14 +21,21 @@ public class GameState : MonoBehaviour
         }
     }
 
-    // 게임 시작 전 5초 준비시간
+    // 게임 시작 전 준비 시간 (카운트 다운)
     IEnumerator FSM_Standby()
     {
-        yield return new WaitForSeconds(GAMESTATE_STANDBYTIME);
+        for(int count = GAMESTATE_STANDBYTIME; count > 0; count--)
+        {
+            Debug.Log("Start count : " + count);
+            yield return new WaitForSeconds(1);
+        }
+
+        //yield return new WaitForSeconds(GAMESTATE_STANDBYTIME);
         Debug.Log("Game Start");
         GameMgr.instance.Set_State(EGameState.FSM_SpawnCount);
     }
 
+    // BattleTimer 0s ~ Xs 사이
     IEnumerator FSM_SpawnCount()
     {
         while (GameMgr.instance.Get_State() == EGameState.FSM_SpawnCount)
@@ -37,6 +44,7 @@ public class GameState : MonoBehaviour
         }
     }
 
+    // 0s가 되는 순간 (SpawnTime)
     IEnumerator FSM_Battle()
     {
         while(GameMgr.instance.Get_State() == EGameState.FSM_Battle)
