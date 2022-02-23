f786e66ccf4296e9917de8b23ca25a69ada87962
Sandor2889 <97664446+Sandor2889@users.noreply.github.com>
2022-02-23 20:10:28 +0900

    Revert "0223 Project 작업"
    This reverts commit 69fa63c3b2580454efe086458cedfde217c5699e.

--- GeoStrike/Assets/Scripts/Unit/Unit.cs
+++ GeoStrike/Assets/Scripts/Unit/Unit.cs
@@ -14,32 +14,20 @@ public class Unit : MonoBehaviourPun
 
     private void Awake()
     {
-        this.gameObject.SetActive(false);
-
         if (photonView.IsMine)
         {
-            this.transform.SetParent(GameObject.Find("Pool_Unit" + this.gameObject.name[0]).transform);
-
-            //for (int idx = 0; idx < 6; idx++)
-            //{
-            //    if (unitIdx - 1 == idx)
-            //    {
-            //        this.transform.SetParent(ObjectPoolMgr.instance.playerPoolArr[PlayerPool.P1].poolArr[idx].transform);
-            //        break;
-            //    }
-            //}
+            for (int idx = 0; idx < 6; idx++)
+            {
+                if (unitIdx - 1 == idx)
+                {
+                    this.transform.SetParent(ObjectPool.instance.poolArr[idx].transform);
+                    break;
+                }
+            }
         }
         else
         {
-            this.transform.SetParent(GameObject.Find("Pool_Unit" + this.gameObject.name[0]).transform);
-            //for (int idx = 0; idx < 6; idx++)
-            //{
-            //    if (unitIdx - 1 == idx)
-            //    {
-            //        this.transform.SetParent(ObjectPoolMgr.instance.playerPoolArr[PlayerPool.P2].poolArr[idx].transform);
-            //        break;
-            //    }
-            //}
+            this.gameObject.SetActive(false);
         }
     }
 
