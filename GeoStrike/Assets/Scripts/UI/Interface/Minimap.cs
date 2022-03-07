16e469b487f6d5640f53e67dc8d069cac4bd18df
Vbeuz <97674706+Vbeuz@users.noreply.github.com>
2022-03-07 21:55:19 +0900

    minimap click 작업중...

--- GeoStrike/Assets/Scripts/UI/Interface/Minimap.cs
+++ GeoStrike/Assets/Scripts/UI/Interface/Minimap.cs
@@ -4,17 +4,52 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class Minimap : MonoBehaviour
+public class Minimap : MonoBehaviour, IPointerClickHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private Camera minimapCamera;
+
+    RectTransform rectTransform;
+
+    public GameObject test;
+
+    public void OnPointerClick(PointerEventData eventData)
     {
-        
+        rectTransform = GetComponent<RectTransform>();
+
+        Ray ray = minimapCamera.ScreenPointToRay(Input.mousePosition);
+        print(ray.origin);
+        test.transform.position = ray.origin;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //public void OnClick()
+    //{
+    //    rectTransform = GetComponent<RectTransform>();
+    //    //Debug.Log("맵 이미지 사이즈 : " + rectTransform.rect.size);
+    //    // (237.3, 237.3)
+
+    //    //이미지의 RectTransform 컴포넌트를 가져온다
+    //    //Debug.Log("맵 이미지 사이즈 : " + rectTransform.offsetMin);
+
+    //    Vector2 mousePos = Input.mousePosition;
+    //    //Debug.Log("마우스를 클릭한 스크린 좌표 : " + mousePos);
+
+    //    Vector2 clickedPos = mousePos - rectTransform.offsetMin;
+
+    //    Vector2 ratioVec = mousePos / rectTransform.rect.size;
+
+    //    Vector2 worldMapPosition;
+    //    worldMapPosition.x = ratioVec.x * rectTransform.rect.size.x;
+    //    worldMapPosition.y = ratioVec.y * rectTransform.rect.size.y;
+
+    //    worldMapPosition = worldMapPosition / 2;
+
+    //    Debug.Log("월드 좌표 : " + worldMapPosition);
+
+    //    mainCamera.transform.position = worldMapPosition;
+    //}
+    //public void OnPointerClick(PointerEventData eventData)
+    //{
+    //    OnClick();
+    //}
 }
