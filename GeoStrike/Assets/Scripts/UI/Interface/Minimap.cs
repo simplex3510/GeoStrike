edc49c01e19ed793812ec7c7a692a3068fe2b8ac
Vbeuz <97674706+Vbeuz@users.noreply.github.com>
2022-03-07 19:39:06 +0900

    minimap 작업중...

--- GeoStrike/Assets/Scripts/UI/Interface/Minimap.cs
+++ GeoStrike/Assets/Scripts/UI/Interface/Minimap.cs
@@ -4,17 +4,47 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class Minimap : MonoBehaviour
+public class Minimap : MonoBehaviour, IPointerClickHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private Camera minimapCamera;
+
+    int zoomSpeed = 1;
+
+    //private void Start()
+    //{
+    //    RaycastHit hit;
+    //    Ray ray = minimapCamera.ScreenPointToRay(Input.mousePosition);
+
+    //    if (Physics.Raycast(ray, out hit))
+    //    {
+    //        Transform objectHit = hit.transform;
+
+
+    //    }
+    //}
+
+    private void Update()
     {
-        
+        Ray ray = minimapCamera.ScreenPointToRay(Input.mousePosition);
+        float zoomDistance = zoomSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+        mainCamera.transform.Translate(ray.direction * zoomDistance, Space.World);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerClick(PointerEventData eventData)
     {
-        
+        //Ray ray = minimapCamera.ScreenPointToRay(Input.mousePosition);
+        //float zoomDistance = zoomSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+        //mainCamera.transform.Translate(ray.direction * zoomDistance, Space.World);
+
+        RaycastHit hit;
+        Ray ray = minimapCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Transform objectHit = hit.transform;
+
+            // Do something with the object that was hit by the raycast.
+        }
     }
 }
