a1e15a2dfe6933e831299cf17de1436083ffd7ea
Sandor2889 <97664446+Sandor2889@users.noreply.github.com>
2022-03-11 21:52:05 +0900

    Tetromino 정보창 만드는 중
    unit에 대한 것과 Tetromino 구별하여 만들기

--- GeoStrike/Assets/Scripts/UI/Interface/StatusPanel.cs
+++ GeoStrike/Assets/Scripts/UI/Interface/StatusPanel.cs
@@ -5,15 +5,16 @@ using UnityEngine.UI;
 
 public class StatusPanel : MonoBehaviour
 {
+    public GameObject clickedObject;
     public Image img;       // 클릭한 오브젝트 이미지
     public Text textName;   // 클릭한 오브젝트 이름
     public Text textHP;     // 클릭한 오브젝트 체력
     public Text textATK;    // 클릭한 오브젝트 공격력
     public Text textDEF;    // 클릭한 오브젝트 방어력
 
-    public void UnitStatusInfo(SpriteRenderer _spriterenderer, string _name, float _hp, float _atk, float _def)
+    public void UnitStatusInfo(SpriteRenderer _spriteRenderer, string _name, float _hp, float _atk, float _def)
     {
-        img.sprite = _spriterenderer.sprite;
+        img.sprite = _spriteRenderer.sprite;
         textName.text = (_name);
         textHP.text = ("HP : " + _hp);
         textATK.text = ("Attack : " + _atk);
@@ -21,8 +22,15 @@ public class StatusPanel : MonoBehaviour
        
     }
 
-    public void TetrominoStatusInfo()
+    public void TetrominoStatusInfo(SpriteRenderer _spriteRenderer, Quaternion _quaternion, string _name)
     {
+        img.sprite = _spriteRenderer.sprite;
+        img.transform.rotation = _quaternion;
+        textName.text = _name;
+    }
 
+    public void NoneStatusInfo()
+    {
+        
     }
 }
