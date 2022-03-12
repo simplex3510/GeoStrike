c04bc2eb7fa45cdf2b6c8b0d246f9f6a01cefb70
이윤석 <59646592+simplex3510@users.noreply.github.com>
2022-03-12 17:43:40 +0900

    End Scene 완료

--- GeoStrike/Assets/Scripts/Manager/ConnectMgr.cs
+++ GeoStrike/Assets/Scripts/Manager/ConnectMgr.cs
@@ -23,16 +23,19 @@ public class ConnectMgr : MonoBehaviourPunCallbacks
     public Button button;
     public Text readyText;
 
-    readonly int MAX_PLAYER = 2;
+    readonly int MAX_PLAYER = 1;
     int readyPlayer = 0;
 
     void Start()
     {
-        button.interactable = false;
-        stateText.text = "마스터 서버에 접속 중";
-        if (!PhotonNetwork.IsConnected)
+        if(!PhotonNetwork.IsConnected)
         {
-            PhotonNetwork.ConnectUsingSettings();
+            button.interactable = false;
+            stateText.text = "마스터 서버에 접속 중";
+            if (!PhotonNetwork.IsConnected)
+            {
+                PhotonNetwork.ConnectUsingSettings();
+            }
         }
     }
 
@@ -73,11 +76,18 @@ public class ConnectMgr : MonoBehaviourPunCallbacks
         button.onClick.AddListener(OnClickReady);
     }
 
-    
-    
+    public void OnLeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnLeftRoom()
+    {
+        PhotonNetwork.LoadLevel("ConnectScene");
+    }
+
     public void OnClickReady()
     {
-        // button.interactable = false;
         photonView.RPC("CurrentReady", RpcTarget.MasterClient, EReadyState.Ready);
         button.onClick.RemoveListener(OnClickReady);
         button.onClick.AddListener(OnClickCancle);
