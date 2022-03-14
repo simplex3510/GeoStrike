fd9aa09b90ffdb6eb74c61ea606d6a17cd5b64b7
simplex3510 <leeyj6457@naver.com>
2022-03-14 23:22:48 +0900

    프로젝트 3D화 작업중

--- GeoStrike/Assets/Scripts/Unit/UnitClass/Buffer/Buff.cs
+++ GeoStrike/Assets/Scripts/Unit/UnitClass/Buffer/Buff.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class Buff : MonoBehaviour
+public class Buff : MonoBehaviourPun
 {
 
     LayerMask opponentLayerMask;
