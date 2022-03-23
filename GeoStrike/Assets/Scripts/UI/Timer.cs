3bd917df8916102a11ad1b35f8264b125804d83f
Sandor2889 <97664446+Sandor2889@users.noreply.github.com>
2022-03-23 19:18:18 +0900

    Timer : PhotonTime으로 수정

--- GeoStrike/Assets/Scripts/UI/Timer.cs
+++ GeoStrike/Assets/Scripts/UI/Timer.cs
@@ -6,20 +6,25 @@ using Photon.Pun;
 using Photon.Realtime;
 
 
-public class Timer : MonoBehaviour, IPunObservable
+public class Timer : MonoBehaviour
 {
     [Header("< World Time >")]
     [SerializeField] private Text worldTimeTXT;
-    public float sec = 0f;
+    public double sec = 0f;
     private int min = 0;
-    private readonly int MAX_SEC = 59;
+    private readonly int MAX_SEC = 60;
 
     [Header("< Battle Timer >")]
     [SerializeField] public float battleTime;
     [SerializeField] private Slider slider;
-    public float battleTimer = 0f;
+    //public float battleTimer = 0f;
+    private int count = 1;
 
-    double sendtime;
+    //double sendtime;
+    double lastTime;    // photon의 시간 고정 값
+    double photonTime;  // 현재 photon 시간
+    double playTime;    // lastTime - photonTime = 현재 게임 플레이한 시간
+
 
     private TranslocateField translocateField;
     private GameState gameState;
@@ -36,10 +41,16 @@ public class Timer : MonoBehaviour, IPunObservable
         slider.maxValue = battleTime;
     }
 
+    private void Start()
+    {
+        lastTime = PhotonNetwork.Time;
+    }
+
     private void Update()
     {
         if (GameMgr.instance.GetState() != EGameState.Standby)
         {
+            photonTime = PhotonNetwork.Time;
             WorldTime();
             BattleTime();
         }
@@ -47,64 +58,71 @@ public class Timer : MonoBehaviour, IPunObservable
 
     private void BattleTime()
     {
-        battleTimer += Time.deltaTime;
-        slider.value = battleTimer;
+        slider.value = (float)playTime;
 
         if (GameMgr.instance.GetState() != EGameState.SpawnCount && GameMgr.instance.GetState() != EGameState.GameEnd)
         {
             GameMgr.instance.SetState(EGameState.SpawnCount);
         }
 
-        if (battleTimer >= battleTime) 
+        if (playTime >= battleTime * count) 
         {
+            count++;
             // (버퍼,디버퍼)유닛의 스킬 선택창 유지 방지
+            #region 스킬 선책창 유지 방지
             if (detector.clickedUnit != null && detector.clickedObject.CompareTag("Unit") && detector.clickedObject.GetComponent<Unit>().unitState == EUnitState.Idle)
             {
                 keySlotPanel.SetActiveFalseAll();
                 detector.clickedUnit = null;
             }
+            #endregion
 
             GameMgr.instance.SetState(EGameState.Battle);
-            battleTimer = 0f;
 
             translocateField.TranslocateUnits();
+
+            // 매 배틀 타임마다 게이지 최소,최대 값 설정
+            slider.minValue = battleTime * (count - 1);
+            slider.maxValue = battleTime * count;
         } 
     }
 
     private void WorldTime()
     {
-        sec += Time.deltaTime;
+        playTime = photonTime - lastTime;
+        sec = playTime - (min * 60);
+      
         worldTimeTXT.text = $"{min:D2} : {(int)sec:D2}";
 
         if(sec >= MAX_SEC)
         {
-            sec = 0;
             min++;
         }
+
     }
 
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        if (stream.IsWriting && PhotonNetwork.IsMasterClient)
-        {
-            sendtime = (float)PhotonNetwork.Time;
-            stream.SendNext(sendtime);
+    //public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    //{
+    //    if (stream.IsWriting && PhotonNetwork.IsMasterClient)
+    //    {
+    //        sendtime = (float)PhotonNetwork.Time;
+    //        stream.SendNext(sendtime);
 
-            stream.SendNext(battleTimer);
-            stream.SendNext(sec);
-            stream.SendNext(min);
-        }
-        else
-        {
-            sendtime = (double)stream.ReceiveNext();
-            float lag = Mathf.Abs((float)(PhotonNetwork.Time - sendtime));
+    //        stream.SendNext(battleTimer);
+    //        stream.SendNext(sec);
+    //        stream.SendNext(min);
+    //    }
+    //    else
+    //    {
+    //        sendtime = (double)stream.ReceiveNext();
+    //        float lag = Mathf.Abs((float)(PhotonNetwork.Time - sendtime));
 
-            battleTimer = (float)stream.ReceiveNext();
+    //        battleTimer = (float)stream.ReceiveNext();
 
-            sec = (float)stream.ReceiveNext();
-            sec += lag;
+    //        sec = (float)stream.ReceiveNext();
+    //        sec += lag;
 
-            min = (int)stream.ReceiveNext();
-        }
-    }
+    //        min = (int)stream.ReceiveNext();
+    //    }
+    //}
 }
